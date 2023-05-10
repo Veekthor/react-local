0082378df19b2713ebde04119fe8cdfadc2c81b4
veekthor <chukailozulu@gmail.com>
2023-05-10 22:21:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Suspense, useState } from "react";
+import i18next from "i18next";
+import { initReactI18next, Trans, useTranslation } from "react-i18next";
+
+const translationEn = {
+  welcome: "Welcome!",
+  sample: "Sample <bold><italics>text</italics></bold>.",
+  changed: "You have changed the language {{count}} time.",
+  changed_plural: "You have changed the language {{count}} times.",
+}
+const translationFr = {
+  welcome: "Bienvenu!",
+  sample: "Exemple de <bold><italics>texte</italics></bold>.",
+  changed: "Vous avez changé la langue {{count}} fois."
+}
+
+i18next
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: translationEn },
+      fr: { translation: translationFr },
+    },
+    lng: "en",
+    fallbackLang: "en",
+    interpolation: { escapeValue:  false },
+  });
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [selectedLang, setLang] = useState("en");
+  const { t, i18n } = useTranslation();
+
+  const onChange = (event) => {
+    const val = event.target.value;
+    setLang(val)
+    setCount(prev => prev + 1);
+    i18n.changeLanguage(val);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback="Loading...">
+      <div className="App">
+        <header className="App-header">
+          <h1>{t('welcome')}</h1>
+          <p>
+            <Trans components={{ /* tell Trans how to process tags that are seen */
+              bold: <strong />,
+              italics: <i />,
+            }}>
+              sample
+            </Trans>
+          </p>
+          <p>
+            {t('changed', { count })}
+          </p>
+          <select name="language" onChange={onChange} value={selectedLang}>
+            <option value="en">English</option>
+            <option value="fr">French</option>
+          </select>
+        </header>
+      </div>
+    </Suspense>
   );
 }
 
